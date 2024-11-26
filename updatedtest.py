cf9213064dc47a3475973652bc9fbcf75cec111d
nitin_shatkar <nitinshetkar53@gmail.com>
2024-11-27 01:25:16 +0530

    code updated

--- updatedtest.py
+++ updatedtest.py
@@ -1,8 +1,9 @@
 """testing functions"""
-from main import add
+from updated import add
 
 def test_add():
     """testing add function"""
     assert add(2,3) == 5
     assert add(0, 42) == 42
     assert add(-1, 1) == 0
+print(add(3,5))
